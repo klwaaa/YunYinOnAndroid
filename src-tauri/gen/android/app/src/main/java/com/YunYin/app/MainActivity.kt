52c190f2c0fc27106f028e4eff0f69c6fb7febc4
klwaaa <2676054134@qq.com>
2026-05-30 10:49:50 +0800

    优化项目结构

--- src-tauri/gen/android/app/src/main/java/com/YunYin/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/YunYin/app/MainActivity.kt
@@ -1,34 +1,11 @@
 package com.YunYin.app
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-    }
-
-    /**
-     * 应用启动时立刻开启前台服务
-     */
-    private fun startForegroundServiceIfNeeded() {
-        val intent = Intent(this, MusicForegroundService::class.java)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            /**
-             * Android 8+
-             * 必须使用 startForegroundService
-             */
-            startForegroundService(intent)
-        } else {
-            /**
-             * Android 7 及以下
-             */
-            startService(intent)
-        }
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
 }
